47434caaccd3b3c46e839b75b03116de0ab9214c
nikos.skl <nikos.skl98@gmail.com>
2019-07-21 23:32:57 +0300

    Add students parser class

--- src/main/java/com/unistudents/api/parser/StudentsParser.java
+++ src/main/java/com/unistudents/api/parser/StudentsParser.java
@@ -1,4 +1,25 @@
 package com.unistudents.api.parser;
 
+import com.unistudents.api.model.GradeResults;
+
 public class StudentsParser {
+
+    private String gradesPage;
+
+    public StudentsParser() {
+    }
+
+    public StudentsParser(String gradesPage) {
+        this.gradesPage = gradesPage;
+    }
+
+    public GradeResults getResults() {
+
+        // get semester
+
+        // ger courses
+
+        // return results
+        return null;
+    }
 }
