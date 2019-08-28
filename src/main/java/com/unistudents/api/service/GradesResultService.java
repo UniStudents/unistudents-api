0975c6a25df899264910976a7be56928541f2659
nikos.skl <nikos.skl98@gmail.com>
2019-08-28 17:17:41 +0300

    [Refactor] Remove unnecessary code & classes

--- src/main/java/com/unistudents/api/service/GradesResultService.java
+++
@@ -1,8 +0,0 @@
-package com.unistudents.api.service;
-
-import com.unistudents.api.model.GradeResults;
-
-public interface GradesResultService {
-
-    public GradeResults getGrades(String username, String password);
-}
