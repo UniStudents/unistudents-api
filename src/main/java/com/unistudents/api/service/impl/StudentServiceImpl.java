776119479b30f2d83646930a1364a94e3b233195
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-09-12 14:46:21 +0300

    Refactor old services

--- src/main/java/com/unistudents/api/service/impl/StudentServiceImpl.java
+++ src/main/java/com/unistudents/api/service/impl/StudentServiceImpl.java
@@ -1,8 +1,8 @@
 package com.unistudents.api.service.impl;
 
 import com.unistudents.api.model.Student;
-import com.unistudents.api.parser.StudentsParser;
-import com.unistudents.api.scraper.StudentsScraper;
+import com.unistudents.api.parser.UNIPIParser;
+import com.unistudents.api.scraper.UNIPIScraper;
 import com.unistudents.api.service.StudentService;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ public class StudentServiceImpl implements StudentService {
     public Student getStudent(String username, String password) {
 
         // scrap info page
-        StudentsScraper scraper = new StudentsScraper(username, password);
+        UNIPIScraper scraper = new UNIPIScraper(username, password);
 
         // authorized check
         if (!scraper.isAuthorized()) {
@@ -24,7 +24,7 @@ public class StudentServiceImpl implements StudentService {
         Document infoPage = scraper.getStudentInfoPage();
         Document gradesPage = scraper.getGradesPage();
 
-        StudentsParser parser = new StudentsParser();
+        UNIPIParser parser = new UNIPIParser();
         return parser.parseInfoAndGradesPages(infoPage, gradesPage);
     }
 }
