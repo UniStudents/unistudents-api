d7857cdd470afcf6f66b18d1be229df5bf18cb82
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-09-12 14:47:04 +0300

    Refactor controllers to support multiple universities

--- src/main/java/com/unistudents/api/web/StudentController.java
+++ src/main/java/com/unistudents/api/web/StudentController.java
@@ -2,14 +2,12 @@ package com.unistudents.api.web;
 
 import com.unistudents.api.model.LoginForm;
 import com.unistudents.api.model.Student;
+import com.unistudents.api.service.ScrapeService;
 import com.unistudents.api.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -18,10 +16,19 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @PostMapping("/student")
-    public ResponseEntity<Student> getStudent(@RequestBody LoginForm loginForm) {
+    @Autowired
+    private ScrapeService scrapeService;
 
-        // get student object from injected service
+    @PostMapping("/student/{university}")
+    public ResponseEntity getStudent(@PathVariable("university") String university, @RequestBody LoginForm loginForm) {
+        return scrapeService.getStudent(university.toUpperCase(), loginForm.getUsername(), loginForm.getPassword());
+    }
+
+    /*
+     *  Support older versions of UniStudents app
+     */
+    @PostMapping("/student")
+    public ResponseEntity getStudentUNIPI(@RequestBody LoginForm loginForm) {
         Student student = studentService.getStudent(loginForm.getUsername(), loginForm.getPassword());
 
         if (student == null) {
