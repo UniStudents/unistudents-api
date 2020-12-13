5ac63b3d2328c715273ffb03295b3d7f215ddcb3
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-12-13 20:13:50 +0200

    Edit controllers

--- src/main/java/com/unistudents/api/web/StudentController.java
+++ src/main/java/com/unistudents/api/web/StudentController.java
@@ -1,11 +1,8 @@
 package com.unistudents.api.web;
 
 import com.unistudents.api.model.LoginForm;
-import com.unistudents.api.model.Student;
 import com.unistudents.api.service.ScrapeService;
-import com.unistudents.api.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,28 +11,13 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
 
     @Autowired
-    private StudentService studentService;
-
-    @Autowired
     private ScrapeService scrapeService;
 
-    @PostMapping("/student/{university}")
-    public ResponseEntity getStudent(@PathVariable("university") String university, @RequestBody LoginForm loginForm) {
-        return scrapeService.getStudent(university.toUpperCase(), loginForm.getUsername(), loginForm.getPassword());
-    }
-
-    /*
-     *  Support older versions of UniStudents app
-     */
-    @PostMapping("/student")
-    public ResponseEntity getStudentUNIPI(@RequestBody LoginForm loginForm) {
-        Student student = studentService.getStudent(loginForm.getUsername(), loginForm.getPassword());
-
-        if (student == null) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        else {
-            return new ResponseEntity<>(student, HttpStatus.OK);
-        }
+    @RequestMapping(value = {"/student/{university}", "/student/{university}/{system}"}, method = RequestMethod.POST)
+    public ResponseEntity getStudent(
+            @PathVariable("university") String university,
+            @PathVariable(required = false) String system,
+            @RequestBody LoginForm loginForm) {
+        return scrapeService.getStudent(university.toUpperCase(), system != null ? system.toUpperCase() : null, loginForm);
     }
 }
