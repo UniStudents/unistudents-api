0f66f203d3a00077cdf1c47e48159f989b4ab852
nickdelis <nikos.dls1@gmail.com>
2021-02-11 21:10:42 +0200

    Scraper updated.

--- src/main/java/com/unistudents/api/scraper/SEFScraper.java
+++ src/main/java/com/unistudents/api/scraper/SEFScraper.java
@@ -18,6 +18,9 @@ public class SEFScraper {
     private final String USER_AGENT;
     private boolean connected;
     private boolean authorized;
+    private Document studentInfoPage;
+    private Document gradesPage;
+    private Map<String, String> cookies;
     private final Logger logger = LoggerFactory.getLogger(SEFScraper.class);
 
     public SEFScraper(LoginForm loginForm) {
@@ -28,7 +31,14 @@ public class SEFScraper {
     }
 
     private void getDocuments(String username, String password, Map<String, String> cookies) {
-        getHtmlPages(username, password);
+        if (cookies == null) {
+            getHtmlPages(username, password);
+        } else {
+            getHtmlPages(cookies);
+            if (studentInfoPage == null || gradesPage == null) {
+                getHtmlPages(username, password);
+            }
+        }
     }
 
     private void getHtmlPages(String username, String password) {
@@ -36,10 +46,10 @@ public class SEFScraper {
         password = password.trim();
 
         Connection.Response response;
-        String PHPSESSION;
+        Map<String, String> cookies;
         boolean authorized;
 
-        // Get PHPSESSID cookie
+        // Get cookies
         try {
             response = Jsoup.connect("https://sef.samos.aegean.gr")
                     .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
@@ -53,7 +63,7 @@ public class SEFScraper {
                     .method(Connection.Method.GET)
                     .execute();
 
-            PHPSESSION = response.cookie("PHPSESSID");
+            cookies = response.cookies();
         } catch (SocketTimeoutException | UnknownHostException | HttpStatusException connException) {
             connected = false;
             logger.warn("Warning: {}", connException.getMessage(), connException);
@@ -63,7 +73,7 @@ public class SEFScraper {
             return;
         }
 
-        // Attempt to connect
+        // Attempt to connect and get grades page
         try {
             response = Jsoup.connect("https://sef.samos.aegean.gr/authentication.php")
                     .data("username", username)
@@ -74,7 +84,6 @@ public class SEFScraper {
                     .header("Connection", "keep-alive")
                     .header("Content-Length", "33")
                     .header("Content-Type", "application/x-www-form-urlencoded")
-                    .header("PHPSESSID", PHPSESSION)
                     .header("DNT", "1")
                     .header("Host", "sef.samos.aegean.gr")
                     .header("Origin", "https://sef.samos.aegean.gr")
@@ -82,6 +91,7 @@ public class SEFScraper {
                     .header("Upgrade-Insecure-Requests", "1")
                     .header("User-Agent", USER_AGENT)
                     .method(Connection.Method.GET)
+                    .cookies(cookies)
                     .execute();
 
             authorized = authorizationCheck(response.parse());
@@ -99,12 +109,140 @@ public class SEFScraper {
             this.authorized = false;
             return;
         }
+
+        // Set grades page
+        try {
+            setGradesPage(response.parse());
+        } catch (IOException e) {
+            logger.error("Error: {}", e.getMessage(), e);
+        }
+
+        // Get info page
+        try {
+            response = Jsoup.connect("https://sef.samos.aegean.gr/request.php")
+                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                    .header("Accept-Encoding", "gzip, deflate, br")
+                    .header("Accept-Language", "el-GR,el;q=0.8,en-US;q=0.5,en;q=0.3")
+                    .header("Connection", "keep-alive")
+                    .header("Content-Length", "33")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("DNT", "1")
+                    .header("Host", "sef.samos.aegean.gr")
+                    .header("Origin", "https://sef.samos.aegean.gr")
+                    .header("Referer", "https://sef.samos.aegean.gr/")
+                    .header("Upgrade-Insecure-Requests", "1")
+                    .header("User-Agent", USER_AGENT)
+                    .method(Connection.Method.GET)
+                    .cookies(cookies)
+                    .execute();
+
+            setStudentInfoPage(response.parse());
+        } catch (SocketTimeoutException | UnknownHostException | HttpStatusException connException) {
+            connected = false;
+            logger.warn("Warning: {}", connException.getMessage(), connException);
+            return;
+        } catch (IOException e) {
+            logger.error("Error: {}", e.getMessage(), e);
+            return;
+        }
+
+        setCookies(cookies);
     }
+
+    private void getHtmlPages(Map<String, String> cookies) {
+        Connection.Response response;
+        boolean authorized;
+
+        // Attempt to connect and get grades page
+        try {
+            response = Jsoup.connect("https://sef.samos.aegean.gr/authentication.php")
+                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                    .header("Accept-Encoding", "gzip, deflate, br")
+                    .header("Accept-Language", "el-GR,el;q=0.8,en-US;q=0.5,en;q=0.3")
+                    .header("Connection", "keep-alive")
+                    .header("Content-Length", "33")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("DNT", "1")
+                    .header("Host", "sef.samos.aegean.gr")
+                    .header("Origin", "https://sef.samos.aegean.gr")
+                    .header("Referer", "https://sef.samos.aegean.gr/")
+                    .header("Upgrade-Insecure-Requests", "1")
+                    .header("User-Agent", USER_AGENT)
+                    .method(Connection.Method.GET)
+                    .cookies(cookies)
+                    .execute();
+
+            authorized = authorizationCheck(response.parse());
+        } catch (SocketTimeoutException | UnknownHostException | HttpStatusException connException) {
+            connected = false;
+            logger.warn("Warning: {}", connException.getMessage(), connException);
+            return;
+        } catch (IOException e) {
+            logger.error("Error: {}", e.getMessage(), e);
+            return;
+        }
+
+        if (response.statusCode() != 200) return;
+
+        // Set grades page
+        try {
+            setGradesPage(response.parse());
+        } catch (IOException e) {
+            logger.error("Error: {}", e.getMessage(), e);
+        }
+
+        // Get info page
+        try {
+            response = Jsoup.connect("https://sef.samos.aegean.gr/request.php")
+                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                    .header("Accept-Encoding", "gzip, deflate, br")
+                    .header("Accept-Language", "el-GR,el;q=0.8,en-US;q=0.5,en;q=0.3")
+                    .header("Connection", "keep-alive")
+                    .header("Content-Length", "33")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("DNT", "1")
+                    .header("Host", "sef.samos.aegean.gr")
+                    .header("Origin", "https://sef.samos.aegean.gr")
+                    .header("Referer", "https://sef.samos.aegean.gr/")
+                    .header("Upgrade-Insecure-Requests", "1")
+                    .header("User-Agent", USER_AGENT)
+                    .method(Connection.Method.GET)
+                    .cookies(cookies)
+                    .execute();
+
+            setStudentInfoPage(response.parse());
+        } catch (SocketTimeoutException | UnknownHostException | HttpStatusException connException) {
+            connected = false;
+            logger.warn("Warning: {}", connException.getMessage(), connException);
+            return;
+        } catch (IOException e) {
+            logger.error("Error: {}", e.getMessage(), e);
+            return;
+        }
+    }
+
+
 
     private boolean authorizationCheck(Document document) {
         String html = document.toString();
 
         return !html.contains("Δεν έχετε δικαίωμα πρόσβασης στο Σ.Ε.Φ. Ελέγξτε τα στοιχεία σας και προσπαθήστε ξανά..");
+    }
+
+    public Document getStudentInfoPage() {
+        return studentInfoPage;
+    }
+
+    private void setStudentInfoPage(Document studentInfoPage) {
+        this.studentInfoPage = studentInfoPage;
+    }
+
+    public Document getGradesPage() {
+        return gradesPage;
+    }
+
+    private void setGradesPage(Document gradesPage) {
+        this.gradesPage = gradesPage;
     }
 
     public boolean isAuthorized() {
@@ -113,5 +251,13 @@ public class SEFScraper {
 
     public boolean isConnected() {
         return connected;
+    }
+
+    public Map<String, String> getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(Map<String, String> cookies) {
+        this.cookies = cookies;
     }
 }
