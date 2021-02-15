1dddf1fb72efdbe46044f17de7336732442cf7c4
nickdelis <nikos.dls1@gmail.com>
2021-02-15 22:45:34 +0200

    Parser: get declared subjects.

--- src/main/java/com/unistudents/api/parser/SEFParser.java
+++ src/main/java/com/unistudents/api/parser/SEFParser.java
@@ -1,14 +1,17 @@
 package com.unistudents.api.parser;
 
 import com.unistudents.api.common.StringHelper;
-import com.unistudents.api.model.Info;
-import com.unistudents.api.model.Student;
+import com.unistudents.api.model.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SEFParser {
 
@@ -43,17 +46,31 @@ public class SEFParser {
         }
     }
 
+    private Grades parseGradesPage(Document gradesPage, int registrationYear) {
+        Grades grades = new Grades();
+        Elements declaredSubjectsDOM = gradesPage.select("#tab_2 > table > tbody > tr");
+        ArrayList<Semester> declaredCourses = getDeclaredCourses(declaredSubjectsDOM, registrationYear);
+
+        Elements passedSubjects = gradesPage.select("#tab_3 table tr");
+
+        return grades;
+    }
+
     public Student parseInfoAndGradesPages(Document infoPage, Document gradesPage) {
         Student student = new Student();
 
         try {
             Info info = parseInfoPage(infoPage, gradesPage);
 
-            if (info == null) {
+            // We have to pass registration year in order to calculate invalid semester value.
+            Grades grades = parseGradesPage(gradesPage, Integer.parseInt(info.getRegistrationYear()));
+
+            if (info == null || grades == null) {
                 return null;
             }
 
             student.setInfo(info);
+            student.setGrades(grades);
 
             return student;
         } catch (Exception e) {
@@ -62,19 +79,93 @@ public class SEFParser {
         }
     }
 
-    // Get registration year from the first subject declaration.
+    // Get all declared courses.
+    private ArrayList<Semester> getDeclaredCourses(Elements declaredSubjectsDOM, int registrationYear) {
+        ArrayList<Semester> declaredSemesters = initSemesters();
+        ArrayList<String> insertedCourses = new ArrayList<>();
+
+        for (int i = declaredSubjectsDOM.size() - 1; i >= 0; i--) {
+            Elements course = declaredSubjectsDOM.get(i).select("td");
+            String courseId = course.get(0).text();
+            String courseExamPeriod = course.get(8).text();
+
+            String semesterId;
+            if ((isSemesterValid(course.get(2).text()))) {
+                semesterId = String.valueOf(Integer.parseInt(course.get(2).text()));
+            ***REMOVED***
+                semesterId = String.valueOf(getSemesterFromExamPeriod(courseExamPeriod, registrationYear));
+            }
+
+            if (!insertedCourses.contains(courseId)) {
+                insertedCourses.add(courseId);
+
+                String courseName = course.get(1).text();
+                String courseType = course.get(5).text();
+
+                Course courseObj = new Course();
+                courseObj.setId(courseId);
+                courseObj.setName(courseName);
+                courseObj.setGrade("-");
+                courseObj.setExamPeriod(courseExamPeriod);
+                courseObj.setType(courseType);
+
+                Semester semester = declaredSemesters.get(Integer.parseInt(semesterId) - 1);
+                semester.getCourses().add(courseObj);
+            }
+        }
+
+        return clearSemesters(declaredSemesters);
+    }
+
+    // Clear unwanted semesters from initialization.
+    private ArrayList<Semester> clearSemesters(ArrayList<Semester> semesters) {
+        Iterator iterator = semesters.iterator();
+        while (iterator.hasNext()) {
+            Semester semester = (Semester) iterator.next();
+            if (semester.getCourses().isEmpty()) {
+                iterator.remove();
+            }
+        }
+
+        return semesters;
+    }
+
+    // Check if semester is valid.
+    private boolean isSemesterValid(String semester) {
+        try {
+            int _semester = Integer.parseInt(semester);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Initialize semesters.
+    private ArrayList<Semester> initSemesters() {
+        Semester[] semesters = new Semester[12];
+        for (int i = 1; i <= 12; i++) {
+            semesters[i - 1] = new Semester();
+            semesters[i - 1].setId(i);
+            semesters[i - 1].setPassedCourses(0);
+            semesters[i - 1].setGradeAverage("-");
+            semesters[i - 1].setCourses(new ArrayList<>());
+        }
+        return new ArrayList<>(Arrays.asList(semesters));
+    }
+
+    // Get registration year from the first course declaration.
     private int getRegistrationYear(Document gradesPage) {
-        Elements firstSubject = gradesPage.select("#tab_2 > table > tbody > tr").get(0).select("td");
-        String firstSubjectRegistration = firstSubject.last().text();
-        return Integer.parseInt(firstSubjectRegistration.replaceAll("\\D+", "").substring(0, 4));
+        Elements firstCourse = gradesPage.select("#tab_2 > table > tbody > tr").get(0).select("td");
+        String firstCourseRegistration = firstCourse.last().text();
+        return Integer.parseInt(firstCourseRegistration.replaceAll("\\D+", "").substring(0, 4));
     }
 
     // Get current student's semester, based on current month and year.
     private int getCurrentSemester(int registrationYear) {
         LocalDate currentDate = LocalDate.now();
         int currentMonth = currentDate.getMonth().getValue();
-        int currentYear = 0;
 
+        int currentYear = 0;
         if (currentMonth >= 1 && currentMonth <= 9) {
             currentYear = currentDate.getYear() - 1;
         ***REMOVED***
@@ -93,10 +184,10 @@ public class SEFParser {
 
     // Get semester from exam period. Necessary for some subjects with semester value: από μαθηματικό.
     private int getSemesterFromExamPeriod(String examPeriod, int registrationYear) {
-        String subjectPeriod = examPeriod.split(" ")[0];
-        int subjectPeriodNumber = ((subjectPeriod.equals("Χειμερινό")) ? 1 : 0);
-        int subjectYear = Integer.parseInt(examPeriod.replaceAll("\\D+", "").substring(0, 4));
-        int currentYear = (subjectYear - registrationYear) + 1;
-        return ((currentYear * 2) - subjectPeriodNumber);
+        String coursePeriod = examPeriod.split(" ")[0];
+        int coursePeriodNumber = ((coursePeriod.equals("Χειμερινό")) ? 1 : 0);
+        int courseYear = Integer.parseInt(examPeriod.replaceAll("\\D+", "").substring(0, 4));
+        int currentYear = (courseYear - registrationYear) + 1;
+        return ((currentYear * 2) - coursePeriodNumber);
     }
 }
